afab609d7458fcfe6275c8db75939087e92dbcb3
jefacb10 <32496187+jefacb10@users.noreply.github.com>
2020-02-20 10:15:06 -0300

    v.02 - desat

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -2,25 +2,21 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Login } from '../models/login.model';
-import { debug } from 'util';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class LoginService {
-  login: Login = new Login();
+
   SEARCH_URL: string = `https://wiseapi-compuwise.azurewebsites.net/service.asmx/login`;
-  PARAMETERS: string = `?brand_Id=58&collection_Id=4&company_Id=1&dept_Id=1`;
+  PARAMETERS: string = `?brand_Id=58&collection_Id=4&company_Id=1&dept_Id=1&`;
 
   constructor(private http: HttpClient) { }
 
-  public getLogin(usuario: string, senha: string):Observable<any> {
-    console.log(this.SEARCH_URL + this.PARAMETERS + `&password=` + senha + `&user=` + usuario); 
-    return this.http.get(
-      this.SEARCH_URL + this.PARAMETERS + `&password=` + senha + `&user=` + usuario
+  public getLogin(usuario: string, senha: string):Observable<Login> {
+    return this.http.get<Login[]>(
+      this.SEARCH_URL + this.PARAMETERS + `password=` + senha + `&user=` + usuario
       );
-      
   }
-
 }
